3b65951374e0ef5a2b789c7411509f399bbecbad
Southworth <JSouthworth@roguecc.edu>
2018-03-01 12:17:14 -0800

    chunking Arrays complete

--- DataStructures_Algo_Exercises/exercises/chunk/index.js
+++ DataStructures_Algo_Exercises/exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   const chunkArr = [],
+//     i = 0,
+//     n = array.length;
+
+//   while (i < n) {
+//     chunkArr.push(array.slice(i, (i += size)));
+//   }
+
+//   return chunkArr;
+// }
+
+// function chunk(array, size) {
+//   const chunkArr = [];
+//   for (let element of array) {
+//     const lastElem = chunkArr[chunkArr.length - 1];
+//     if (!lastElem || lastElem.length === size) {
+//       chunkArr.push([element]);
+//     } else {
+//       lastElem.push(element);
+//     }
+//   }
+//   return chunkArr;
+// }
+
+function chunk(array, size) {
+  let chunkArr = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunkArr.push(array.slice(i, size + i));
+  }
+
+  return chunkArr;
+}
 
 module.exports = chunk;
